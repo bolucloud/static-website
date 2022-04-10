ce3ac9759e2f0d67fba95b02a6adbad33ab3b764
bolucloud <bolu@bolu.cloud>
2022-04-09 20:44:01 -0500

    fix

--- s3.tf
+++ s3.tf
@@ -2,7 +2,6 @@
 
 resource "aws_s3_bucket" "static-website-bucket" {
   bucket = "www.${var.bucket_name}"
-  policy = data.aws_iam_policy_document.website_policy.json
   tags   = var.common_tags
 }
 
